174529592269d2f9db48fe6bc8649207029ecda5
david-andreasson <davidandreasson@live.com>
2024-09-05 15:01:53 +0200

    Exercises from Thursday

---
+++ 1_oop_basics/lecture_4/src/Exercises_2/md/Main.java
@@ -0,0 +1,14 @@
+package Exercises_2.md;
+
+public class Main {
+    public static void main(String[] args) {
+Student student = new Student();
+student.setName("James");
+student.setAge(23);
+student.setGrade('B');
+System.out.println(student.getAge());
+
+
+
+    }
+}
---
+++ 1_oop_basics/lecture_4/src/Exercises_2/md/Student.java
@@ -0,0 +1,26 @@
+package Exercises_2.md;
+
+public class Student {
+    private String name;
+    private int age;
+    private char grade;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age > 0) this.age = age;
+    }
+    public char getGrade() {
+        return grade;
+    }
+    public void setGrade(char grade) {
+        if (grade > 0) this.grade = grade;
+    }
+}
