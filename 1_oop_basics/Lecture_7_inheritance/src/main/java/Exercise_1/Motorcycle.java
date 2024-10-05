cb7a1f1d246c78ef4598a92966ba791ad520411f
david-andreasson <79davand@gafe.molndal.se>
2024-10-05 13:57:51 +0200

    Clean up

---
+++ 1_oop_basics/Lecture_7_inheritance/src/main/java/Exercise_1/Motorcycle.java
@@ -0,0 +1,7 @@
+package Exercise_1;
+
+public class Motorcycle extends Vehicle {
+    public void popWheelie() {
+        System.out.println("Motorcycle popping a wheelie");
+    }
+}
---
+++ 1_oop_basics/Lecture_7_inheritance/src/main/java/Exercise_1/Vehicle.java
@@ -0,0 +1,15 @@
+package Exercise_1;
+
+public class Vehicle {
+    public String make;
+    public String model;
+    public int year;
+
+    public void startEngine() {
+        System.out.println("Starting engine");
+    }
+    public void stopEngine() {
+        System.out.println("Stopping engine");
+    }
+
+}
