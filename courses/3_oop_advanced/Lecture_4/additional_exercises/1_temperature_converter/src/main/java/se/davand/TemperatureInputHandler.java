12abb731d9c74d9b8b42315582c6b529b13f8aff
david-andreasson <79davand@gafe.molndal.se>
2024-11-10 11:51:11 +0100

    Removed logic from TemperatureInputHandler

--- courses/3_oop_advanced/Lecture_4/additional_exercises/1_temperature_converter/src/main/java/se/davand/TemperatureInputHandler.java
+++ courses/3_oop_advanced/Lecture_4/additional_exercises/1_temperature_converter/src/main/java/se/davand/TemperatureInputHandler.java
@@ -1,19 +1,5 @@
 package se.davand;
 
 public class TemperatureInputHandler {
-    private double temperature;
-    private String unit;
 
-    public TemperatureInputHandler(double temperature, String unit) {
-        this.temperature = temperature;
-        this.unit = unit;
     }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-}
