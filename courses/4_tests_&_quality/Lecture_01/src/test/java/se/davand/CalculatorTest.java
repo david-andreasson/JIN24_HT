7d1257d09d1e8bfc8eae40eb6e626c8900ab8e90
david-andreasson <79davand@gafe.molndal.se>
2024-12-04 08:59:26 +0100

    Lecture 1 - Exercise 2 - added support for parameterized tests in Pomfile -added two parameterized test in CalculatorTest

--- courses/4_tests_&_quality/Lecture_01/src/test/java/se/davand/CalculatorTest.java
+++ courses/4_tests_&_quality/Lecture_01/src/test/java/se/davand/CalculatorTest.java
@@ -3,19 +3,23 @@ package se.davand;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
     @BeforeEach
     void setUp() {
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
     }
 
     @AfterEach
     void tearDown() {
-        Object calculator = null;
+        calculator = null;
     }
 
     @Test
@@ -37,4 +41,25 @@ class CalculatorTest {
     void divide() {
         assertEquals(4, Calculator.divide(8, 2));
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,2,3",
+            "2,2,4",
+            "-3,2,-1"
+    })
+    void ParameterizedAdd(int a, int b, int expected) {
+        assertEquals(expected, Calculator.add(a, b));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,2,-1",
+            "2,2,0",
+            "4,8,-4"
+    })
+    void ParameterizedSubtract(int a, int b, int expected) {
+        assertEquals(expected, Calculator.subtract(a, b));
+    }
+
 }
