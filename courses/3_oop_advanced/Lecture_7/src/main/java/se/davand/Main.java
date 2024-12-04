77d5057da6bfad1e92981a5b66b76cf607de659a
david-andreasson <79davand@gafe.molndal.se>
2024-12-04 05:51:59 +0100

    Lecture 7 files

--- courses/3_oop_advanced/Lecture_7/src/main/java/se/davand/Main.java
+++ courses/3_oop_advanced/Lecture_7/src/main/java/se/davand/Main.java
@@ -1,7 +1,17 @@
 package se.davand;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Etablera anslutning till MongoDB
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+// Välj databas
+        MongoDatabase database = mongoClient.getDatabase("mydatabase");
+// Välj collection
+        MongoCollection<Document> collection = database.getCollection("users");
     }
+
 }
