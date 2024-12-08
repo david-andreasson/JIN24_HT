cead8a4b8762905b39d6be4111acd36721e4653f
david-andreasson <79davand@gafe.molndal.se>
2024-12-08 08:43:57 +0100

    Lecture 2 - 2_exercises.md

---
+++ courses/4_tests_&_quality/Lecture_02/src/test/java/se/davand/BasicTestSuite.java
@@ -0,0 +1,12 @@
+package se.davand;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.example.tests")
+public class BasicTestSuite {
+
+}
+
+
