be7d735ce1f76d416a30f7b46096682916ef66e1
david-andreasson <79davand@gafe.molndal.se>
2024-12-12 04:36:55 +0100

    Lecture 3 extra_exercises from chatgpt

--- courses/4_tests_&_quality/Lecture_03/src/test/java/se/davand/extra_exercises/TestPasswordValidation.java
+++ courses/4_tests_&_quality/Lecture_03/src/test/java/se/davand/extra_exercises/TestPasswordValidation.java
@@ -11,4 +11,16 @@ public class TestPasswordValidation {
         PasswordValidation passwordValidation = new PasswordValidation();
         assertEquals(true, passwordValidation.isPasswordValid("password1"));
     }
+
+    @Test
+    public void testPasswordWithLessThenEightChars() {
+        PasswordValidation passwordValidation = new PasswordValidation();
+        assertEquals(false, passwordValidation.isPasswordValid("passwo1"));
+    }
+
+    @Test
+    public void testPasswordWithoutNumber() {
+        PasswordValidation passwordValidation = new PasswordValidation();
+        assertEquals(false, passwordValidation.isPasswordValid("password"));
+    }
 }
