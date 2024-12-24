a3465030987daf8baeb70b8685fa5f9f2a787bdf
david-andreasson <79davand@gafe.molndal.se>
2024-12-24 05:08:15 +0100

    Add basic Book class

---
+++ courses/extra_exercises/01_Library/src/main/java/se/davand/Book.java
@@ -0,0 +1,32 @@
+package se.davand;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
