b8560f5a518c0dc76673c350a14feb48a9162663
david-andreasson <79davand@gafe.molndal.se>
2024-12-25 12:41:50 +0100

    Added some base methods to Restaurant

--- courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Restaurant.java
+++ courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Restaurant.java
@@ -5,10 +5,35 @@ import java.util.ArrayList;
 public class Restaurant {
 
     //Lista för att lagra bokningar
-    ArrayList reservations = new ArrayList<>();
+    ArrayList<String> reservations = new ArrayList<>();
 
     //Metod för att lägga till bokning
-    //Metod för att ta bort boking
+    public void addReservation() {
+        reservations.add(reservation);
+    }
+
+    //Metod för att ta bort bokning
+    public void removeReservation() {
+        reservations.remove(reservation);
+    }
+
     //Metod för att lista alla bokningar
+    public void listAllReservations() {
+        for (String reservation : reservations) {
+            System.out.println(reservation);
+        }
+    }
+
     //Metod för att söka efter en bokning baserat på namn
+    public void searchForReservation() {
+        for (String reservation : reservations) {
+        if (reservations.contains(nameToSearchFor)){
+            System.out.println(reservation);
+        }
+        else {
+            System.out.println("Could not find reservation");
+        }
+
+        }
+    }
 }
