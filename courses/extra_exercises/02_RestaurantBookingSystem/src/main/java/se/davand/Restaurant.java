9815e23c58daf14128eac6f9967ee97e1265dcfb
david-andreasson <79davand@gafe.molndal.se>
2024-12-25 13:00:14 +0100

    Time is 19:00 (Vietnam time) on Christmas day, im clocking out

--- courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Restaurant.java
+++ courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Restaurant.java
@@ -1,32 +1,46 @@
 package se.davand;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 
 public class Restaurant {
 
+
     //Lista för att lagra bokningar
-    ArrayList<String> reservations = new ArrayList<>();
+    ArrayList<Reservation> reservations = new ArrayList<>();
 
     //Metod för att lägga till bokning
-    public void addReservation() {
-        reservations.add(reservation);
+    public void addReservation(String name, int numberOfPeople, String date) {
+        Reservation newReservation = new Reservation(name, numberOfPeople, date);
+        reservations.add(newReservation);
     }
 
     //Metod för att ta bort bokning
-    public void removeReservation() {
-        reservations.remove(reservation);
+    public void removeReservation(String reservationToDelete) {
+        boolean foundReservation = false;
+        for (Reservation reservation : reservations) {
+            if (reservation.getName().equalsIgnoreCase(reservationToDelete)) {
+                System.out.println("Removing: " + reservation.getName()+"-reservation");
+                reservation.remove(reservation);
+                foundReservation = true;
+                break;
+            }
+        }
+        if (!foundReservation) {
+            System.out.println("Could not find reservation in booking system");
+        }
     }
 
     //Metod för att lista alla bokningar
     public void listAllReservations() {
-        for (String reservation : reservations) {
+        for (Reservation reservation : reservations) {
             System.out.println(reservation);
         }
     }
 
     //Metod för att söka efter en bokning baserat på namn
     public void searchForReservation() {
-        for (String reservation : reservations) {
+        for (Reservation reservation : reservations) {
         if (reservations.contains(nameToSearchFor)){
             System.out.println(reservation);
         }
