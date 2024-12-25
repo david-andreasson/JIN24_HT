eab179eb9635f0d84c2bf28d2de41dd613387d53
david-andreasson <79davand@gafe.molndal.se>
2024-12-25 12:11:03 +0100

    Add made reservation class

--- courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Reservation.java
+++ courses/extra_exercises/02_RestaurantBookingSystem/src/main/java/se/davand/Reservation.java
@@ -1,4 +1,38 @@
 package se.davand;
 
 public class Reservation {
+
+    private String Name;
+    private int numberOfPeople;
+    private String date;
+
+    public Reservation(String name, int numberOfPeople, String date) {
+        Name = name;
+        this.numberOfPeople = numberOfPeople;
+        this.date = date;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
